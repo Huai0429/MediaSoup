07030e2a3f0cf662b49d595446d872f60ad5a5a0
Huai-Server <huai0429.gcp@gmail.com>
2023-03-22 23:12:51 +0800

    2022/03/22 1.Create Woker as RtcPort from 2000 to 2020 2.Create Router as media Codecs

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import path from 'path'
 const __dirname = path.resolve()
 
 import {Server} from 'socket.io'
+import mediasoup from 'mediasoup'
 
 app.get('/',(req,res)=>{
     res.send('Hello from mediasoup app')
@@ -30,10 +31,59 @@ const io = new Server(httpsServer)
 
 const peers = io.of('/mediasoup')
 
-peers.on('connection' , socket => { //'connection' event on peers
+
+
+
+//worker 
+
+let worker 
+//worker's RTCport
+const  createWorker = async()=>{
+    worker = await mediasoup.createWorker({
+        rtcMinPort: 2000,
+        rtcMaxPort: 2020,
+    })
+    console.log(`worker pid ${worker.pid}`)
+
+    worker.on('died',error => {
+        console.error('mediasoup worker has died')
+        setTimeout(()=> process.exit(1),2000)//exit in 2 sec
+    })
+    return worker
+}
+worker = createWorker()
+
+const mediaCodecs = [
+    {
+        kind: 'audio',
+        mimeType: 'audio/opus',
+        clockRate:48000,
+        channels:2,
+    },
+    {
+        kind:'video',
+        mimeType:'video/VP8',
+        clockRate:90000,
+        parameters:{
+            'x-google-start-bitrate':1000,
+        },
+    },
+]
+
+//router
+let router 
+
+//client connect & disconnect
+peers.on('connection' , async socket => { //'connection' event on peers
     console.log('Connection Event from app.js'),
     console.log(socket.id)
     socket.emit('connection-success',{ //emit back 'connection-success' link to index.js
         socketId: socket.id
     })
+
+    socket.on('disconnect',()=>{
+        //do some clean up
+        console.log('peer disconnect')
+    })
+    router = await worker.createRouter({mediaCodecs})
 })
